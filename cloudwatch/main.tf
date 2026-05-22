a73860326cb0b9294f3a6a07c4a9d534f8cfe833
yuki <yu_uy.6.king.7@icloud.com>
2026-05-22 16:26:01 +0900

    feat: CloudWatch Dashboardモジュール追加

---
+++ cloudwatch/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "${var.project_name}-dashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          title  = "Lambda エラー数"
+          period = 300
+          stat   = "Sum"
+          metrics = [
+            ["AWS/Lambda", "Errors", "FunctionName", "${var.project_name}-ingest"],
+            ["AWS/Lambda", "Errors", "FunctionName", "${var.project_name}-query"],
+            ["AWS/Lambda", "Errors", "FunctionName", "${var.project_name}-presigned-url"]
+          ]
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          title  = "Lambda 実行時間"
+          period = 300
+          stat   = "Average"
+          metrics = [
+            ["AWS/Lambda", "Duration", "FunctionName", "${var.project_name}-ingest"],
+            ["AWS/Lambda", "Duration", "FunctionName", "${var.project_name}-query"],
+            ["AWS/Lambda", "Duration", "FunctionName", "${var.project_name}-presigned-url"]
+          ]
+        }
+      },
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 12
+        height = 6
+        properties = {
+          title  = "API Gateway レイテンシ"
+          period = 300
+          stat   = "Average"
+          metrics = [
+            ["AWS/ApiGateway", "Latency", "ApiName", "${var.project_name}-api"]
+          ]
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 6
+        width  = 12
+        height = 6
+        properties = {
+          title  = "API Gateway 4XXエラー"
+          period = 300
+          stat   = "Sum"
+          metrics = [
+            ["AWS/ApiGateway", "4XXError", "ApiName", "${var.project_name}-api"]
+          ]
+        }
+      }
+    ]
+  })
+}
