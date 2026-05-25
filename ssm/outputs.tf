f7876d39a3fbafc7bd1665f5745b7182402176b6
yuki <yu_uy.6.king.7@icloud.com>
2026-05-25 13:33:28 +0900

    fix: SSMモジュールのoutputs.tf修正

--- ssm/outputs.tf
+++ ssm/outputs.tf
@@ -1,10 +1,4 @@
-resource "aws_ssm_parameter" "vector_store_endpoint" {
-  name        = "/rp/${var.environment}/vector-store/endpoint"
-  type        = "SecureString"
-  value       = var.vector_store_endpoint
-  description = "ベクトルストアのエンドポイント"
-
-  tags = {
-    Name = "${var.project_name}-vector-store-endpoint"
-  }
+output "vector_store_endpoint_param_name" {
+  value       = aws_ssm_parameter.vector_store_endpoint.name
+  description = "SSMパラメータ名"
 }
