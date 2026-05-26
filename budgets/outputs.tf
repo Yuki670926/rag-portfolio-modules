4efa44efb11a3a5241d32d4d25e5a7ac5df3ee5d
yuki <yu_uy.6.king.7@icloud.com>
2026-05-26 10:12:19 +0900

    feat: budgetsモジュールにalert_email output追加

--- budgets/outputs.tf
+++ budgets/outputs.tf
@@ -1,3 +1,12 @@
 output "budget_name" {
   value = aws_budgets_budget.monthly.name
 }
+
+output "budget_name" {
+  value = aws_budgets_budget.monthly.name
+}
+
+output "alert_email" {
+  value     = jsondecode(data.aws_secretsmanager_secret_version.alert_email.secret_string)["email"]
+  sensitive = true
+}
