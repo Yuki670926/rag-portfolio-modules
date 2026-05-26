6a98d5df573e0db3a768ccaab27851d12db2af7d
yuki <yu_uy.6.king.7@icloud.com>
2026-05-26 11:07:47 +0900

    fix: DLQキュー名の重複修正

--- dlq/main.tf
+++ dlq/main.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "dlq" {
-  name                      = "${var.project_name}-${var.environment}-dlq-${var.queue_name_suffix}"
+  name                      = "${var.project_name}-dlq-${var.queue_name_suffix}"
   message_retention_seconds = var.message_retention_seconds
   kms_master_key_id         = "alias/aws/sqs"
 
   tags = {
-    Name        = "${var.project_name}-${var.environment}-dlq-${var.queue_name_suffix}"
+    Name        = "${var.project_name}-dlq-${var.queue_name_suffix}"
     Environment = var.environment
     ManagedBy   = "terraform"
   }
